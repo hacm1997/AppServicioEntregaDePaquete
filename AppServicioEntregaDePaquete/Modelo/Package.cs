e06171a02a6b8c4cbc4d3208ad44f4f859a9961e
hacm97 <h.acostas@live.com>
2015-03-04 16:01:15 -0500

    edit

--- AppServicioEntregaDePaquete/Modelo/Package.cs
+++ AppServicioEntregaDePaquete/Modelo/Package.cs
@@ -13,6 +13,7 @@ namespace AppServicioEntregaDePaquete.Modelo
         private string ciudad;
         private string estado;
         private string pesoPaquete;
+        private string costoPaquete;
 
         public string Codigo
         {
@@ -50,6 +51,12 @@ namespace AppServicioEntregaDePaquete.Modelo
             set { pesoPaquete = value; }
         }
 
+        public string CostoPaquete
+        {
+            get { return costoPaquete; }
+            set { costoPaquete = value; }
+        }
+
         public Package() 
         {
             this.codigo = "XXXXXX";
@@ -57,12 +64,49 @@ namespace AppServicioEntregaDePaquete.Modelo
             this.direccion = "DDDDDD";
             this.ciudad = "City";
             this.estado = "N.N";
-            this.pesoPaquete = "X-KG.";
+            this.pesoPaquete = "50 Kg";
+            this.costoPaquete = "50.000";
         }
 
         public Package(string codigo, string nombre, string direccion, string ciudad, string estado, string pesoPaquete) 
         {
+            this.codigo = codigo;
+            this.nombre = nombre;
+            this.direccion = direccion;
+            this.ciudad = ciudad;
+            this.estado = estado;
+            this.pesoPaquete = pesoPaquete;
+        }
 
+        public override string ToString()
+        {
+            return "\n=================================\n" +
+            "Código: " + this.codigo + "\n" +
+            "Nombre: " + this.nombre + "\n" +
+            "Dirección: " + this.direccion + "\n" +
+            "Ciudad: " + this.ciudad + "\n" +
+            "Estado: " + this.estado + "\n" +
+            "Peso Del Paquete en Kg: " + this.pesoPaquete + "\n" +
+            "Costo Del Paquete: " + this.costoPaquete;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            Package o = (Package)obj;
+            bool result = false;
+
+            if ((this.codigo == o.codigo) &&
+                (this.nombre == o.nombre) && (this.direccion == o.direccion) &&
+                (this.ciudad == o.ciudad) && (this.estado == o.estado) &&
+                (this.pesoPaquete == o.pesoPaquete) && (this.costoPaquete == o.costoPaquete))
+                result = true;
+
+            return result;
         }
 
     }
