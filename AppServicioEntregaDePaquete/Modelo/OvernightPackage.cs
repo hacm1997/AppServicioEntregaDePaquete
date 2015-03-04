7f1a93358fb8396381131166d6e490262e75fe90
hacm97 <h.acostas@live.com>
2015-03-04 16:36:33 -0500

    Parcial

---
+++ AppServicioEntregaDePaquete/Modelo/OvernightPackage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppServicioEntregaDePaquete.Modelo
+{
+    public class OvernightPackage : Package
+    {
+        private double costoEntregaNoche;
+        private double calculateCost3;
+
+        public double CalculateCost3
+        {
+            get { return calculateCost3; }
+            set { calculateCost3 = value; }
+        }
+
+        public double CostoEntregaNoche
+        {
+            get { return costoEntregaNoche; }
+            set { costoEntregaNoche = value; }
+        }
+
+    }
+}
--- AppServicioEntregaDePaquete/Modelo/TwoDayPackage.cs
+++ AppServicioEntregaDePaquete/Modelo/TwoDayPackage.cs
@@ -8,6 +8,13 @@ namespace AppServicioEntregaDePaquete.Modelo
     public class TwoDayPackage : Package
     {
         private double cuotaServicioEntregaDosDias;
+        private double calculateCost2;
+
+        public double CalculateCost2
+        {
+            get { return calculateCost2; }
+            set { calculateCost2 = value; }
+        }
 
         public double CuotaServicioEntregaDosDias
         {
@@ -18,17 +25,22 @@ namespace AppServicioEntregaDePaquete.Modelo
         public TwoDayPackage() 
         {
             this.cuotaServicioEntregaDosDias = 100000;
+            this.CalculateCost2 = this.CalculateCost + this.cuotaServicioEntregaDosDias;
         }
 
         public TwoDayPackage(double coutaServicioEntregaDosDias) 
         {
             this.cuotaServicioEntregaDosDias = cuotaServicioEntregaDosDias;
+            this.CalculateCost2 = CalculateCost2;
         }
 
         public override string ToString()
         {
             return "\n=================================\n" +
-            "Cuota Total del Envio con el servicio de entre de 2 dias: " + this.cuotaServicioEntregaDosDias;
+            "Cuota del servicio de entrega de 2 días: " + this.cuotaServicioEntregaDosDias + "\n" +
+            "Cuota Total del Envio con el servicio de entre de 2 dias: " + this.CalculateCost2;
         }
+
+
     }
 }
